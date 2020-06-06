4b63b68f6a808e948ccfed5ae8b2aa8ffc3f1c64
lucaszw <lucas.zeer@gmail.com>
2020-06-06 15:30:07 -0400

    and trends

---
+++ public/api.js
@@ -0,0 +1,27 @@
+window.API = {};
+
+API.postResults = async (results) => {
+    console.log({results}, JSON.stringify({results}));
+    const url = '/results';
+    const options = {
+        method: 'POST',
+        body: JSON.stringify({results}),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    // send POST request
+    return await fetch(url, options).then(res => res.json())
+}
+
+API.getTrends = async () => {
+    const url = '/trends';
+    const options = {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }
+    return await fetch(url, options).then(res => res.json())
+}
+
--- server.js
+++ server.js
@@ -8,14 +8,30 @@ const app = express()
 app.use(bodyParser.json())
 app.use(express.static('public'))
 
-app.post('/result', (req, res) => {
-    console.log("Posting results", res.body);
-})
+app.post('/results', async (req, res) => {
+    await db.survey.create({results});
+    res.send({status: 'ok'});
+});
 
 app.get('/results', async (req, res) => {
     let surveys = await db.survey.findAll();
-    console.log(surveys);
     res.send(surveys);
+});
+
+app.get('/trends', async (req, res) => {
+    let [popularity] = await db.sequelize.query(`
+        with categories as (
+            select
+                unnest(results) as category
+            from surveys
+        )
+        select
+            category,
+            count(category) as popularity
+        from categories
+        group by category
+    `);
+    res.send(popularity);
 });
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
