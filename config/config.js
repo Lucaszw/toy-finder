4e0a1ca89b8214dc08cb9306d8f264ee930e1aeb
lucaszw <lucas.zeer@gmail.com>
2020-06-06 20:33:32 -0400

    add database and use_env to production config

--- config/config.js
+++ config/config.js
@@ -9,9 +9,14 @@ module.exports = {
   },
   "production": {
     "url": process.env.DATABASE_URL,
+    "database": process.env.DATABASE_URL,
     "dialectOptions": {
-      "ssl": false
+      "ssl": {
+        "require": true,
+        "rejectUnauthorized": false
+      }
     },
-    "dialect": "postgres"
+    "dialect": "postgres",
+    "use_env_variable": "DATABASE_URL"
   }
 }
